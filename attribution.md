a9c14b328d2b56fecc69454b8ba333b3ca4d2a11
Euan <euan.goddard@gmail.com>
2014-09-15 22:17:07 +0100

    Added first set of controls

--- attribution.md
+++ attribution.md
@@ -4,3 +4,4 @@ All icons licensed under Creative Commons Attribution
 - Present by Riccardo Rossetti from The Noun Project
 - Deer by Benni from The Noun Project
 - Santa Claus by Darren Geraghty from The Noun Project
+- Sleigh by Laura Beggs from The Noun Project
--- js/main.js
+++ js/main.js
@@ -3,13 +3,52 @@
 
     var xmas = angular.module('xmas', ['angularMoment', 'ticker']);
 
+
     xmas.controller('GameCtrl', function ($scope, Ticker) {
         $scope.ticks = [];
         $scope.is_running = false;
         $scope.date = new Date(2013, 11, 26);
+        $scope.presents = 0;
+        $scope.items = [
+            {
+                id: 'elf',
+                cost: 10,
+                label: 'Elf',
+                effect: 10,
+                quantity: 0
+            },
+            {
+                id: 'reindeer',
+                cost: 100,
+                label: 'Reindeer',
+                effect: 20,
+                quantity: 0
+            },
+            {
+                id: 'santa',
+                cost: 1000,
+                label: 'Santa upgrade',
+                effect: 30,
+                quantity: 0
+            },
+            {
+                id: 'sleigh',
+                cost: 10000,
+                label: 'Sleigh upgrade',
+                effect: 40,
+                quantity: 0
+            }
+        ];
+
         Ticker.tick(function () {
             if ($scope.is_running) {
                 $scope.date = moment($scope.date).add(1, 'hours');
+
+                var presents_to_increment = 0;
+                angular.forEach($scope.items, function (item) {
+                    presents_to_increment += (item.effect * item.quantity);
+                });
+                $scope.presents += presents_to_increment;
             }
         });
 
@@ -19,6 +58,17 @@
 
         this.pause = function () {
             $scope.is_running = false;
+        };
+
+        this.make_present = function () {
+            $scope.presents += 1;
+        };
+
+        this.buy_item = function (item) {
+            if (item.cost <= $scope.presents) {
+                $scope.presents -= item.cost;
+                item.quantity += 1;
+            }
         };
         
     });
